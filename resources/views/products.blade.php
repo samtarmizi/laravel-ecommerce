623e325b524bedbcd566efd2ac87d9c88fba260f
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2019-09-06 17:16:46 +0800

    Update routes

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,7 +10,7 @@
                         <img src="{{ $product->photo }}" width="500" height="300">
                         <div class="caption">
                             <h4>{{ $product->name }}</h4>
-                            <p>{{ str_limit(strtolower($product->description), 50) }}</p>
+                            <p>{{ $product->description }}</p>
                             <p><strong>Price: </strong> {{ $product->price }}$</p>
                             <p class="btn-holder"><a href="{{ url('add-to-cart/'.$product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
                         </div>
